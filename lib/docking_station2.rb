d2c019ae0d71952d3ede62442aad4007f47f5e86
Jack <jack3009@hotmail.co.uk>
2020-10-15 15:05:31 +0100

    15 - Single Responsibility Principle

--- lib/docking_station2.rb
+++ lib/docking_station2.rb
@@ -8,13 +8,12 @@ attr_reader :dock_array, :number_of_bikes
   end
 
   def release
-    raise "There are no bikes" if @dock_array.length == 0
+    raise "There are no bikes" if empty?
     @bike = Bike.new; @dock_array.pop if @dock_array.length != 0
   end
 
-
-  def return_bike
-    if @dock_array.length < @capacity then @dock_array << Bike.new  else raise "The docking station is full" end
+  def return_bike(bike)
+    if full? then @dock_array << bike  else raise "The docking station is full" end
   end
 
   def see_bike
@@ -23,6 +22,16 @@ attr_reader :dock_array, :number_of_bikes
     else
       return "There are no bikes to see"
     end
+  end
+
+  private
+
+  def full?
+    return (if @dock_array.length < @capacity then true else false end)
+  end
+
+  def empty?
+    return (if @dock_array.length == 0 then true else false end)
   end
 
 end
@@ -34,23 +43,5 @@ attr_reader :bike
   def working?
     return "This bike is working"
   end
-
-end
-
-def release
-  if @dock_array.length == 0
-    raise "There are no bikes"
-  else
-    @bike = Bike.new
-    @dock_array.pop
-  end
-end
-
-def return_bike
-  if @dock_array.length < @capacity
-   @dock_array << "bike"
- else
-   raise "The docking station is full"
- end
 
 end
--- spec/docking_station2_spec.rb
+++ spec/docking_station2_spec.rb
@@ -6,7 +6,7 @@ describe DockingStation do
   describe "#initialize" do
     it 'counts to make sure we have 20 bikes' do
       station = DockingStation.new
-      20.times {station.return_bike}
+      20.times {station.return_bike(Bike.new)}
       actual_value = station.dock_array.length
       expected_value = 20
       expect(actual_value). to eq expected_value
@@ -16,7 +16,7 @@ describe DockingStation do
   describe '#release' do
     it 'releases a bike' do
       station = DockingStation.new
-      20.times {station.return_bike}
+      20.times {station.return_bike(Bike.new)}
       station.release
       expect(station.dock_array.length).to eq 19
     end
@@ -32,7 +32,7 @@ describe DockingStation do
   describe '#return_bike' do
     it 'adds a bike to the dock' do
       station = DockingStation.new
-      station.return_bike
+      station.return_bike(Bike.new)
       expect(station.dock_array.length).to eq 1
     end
   end
@@ -40,14 +40,14 @@ describe DockingStation do
   describe '#return_bike' do
     it 'doesnt add a bike to the dock, raises error' do
       station = DockingStation.new(0)
-      expect{station.return_bike}.to raise_error("The docking station is full")
+      expect{station.return_bike(Bike.new)}.to raise_error("The docking station is full")
     end
   end
 
   describe '#see_bike' do
     it 'checks to see if there is a bike in the dock to release' do
       station = DockingStation.new
-      station.return_bike
+      station.return_bike(Bike.new)
       expect(station.see_bike).to eq "There is a bike"
     end
   end
